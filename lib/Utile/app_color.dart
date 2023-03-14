c9d8295ef78a6382c798c923cec427735b990991
Arshadkhan247 <ak4322866@gmail.com>
2023-03-15 02:15:31 +0500

    color adding

--- lib/Utile/app_color.dart
+++ lib/Utile/app_color.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 Color appColor = const Color.fromARGB(255, 95, 127, 202);
 Color backgroundColor = const Color.fromARGB(255, 241, 249, 255);
+Color primaryColor = const Color.fromARGB(255, 0, 0, 0);
